72f1177543c2ba367826b9cf9bac727a45480ded
Rigwarl <Avips@mail.ru>
2020-07-16 23:31:25 +0300

    add docs folder

---
+++ docs/precache-manifest.8b9f8649aed2d77738dc8bc21d28ed3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554221609baf2be4fdc60b4bf176d54c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b9224b712950eab7a1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "76f758cfdd9a1344c2dd",
+    "url": "/static/js/2.4cfa2dca.chunk.js"
+  },
+  {
+    "revision": "65134ee3c131cee81edc1f90cfd5bf05",
+    "url": "/static/js/2.4cfa2dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b9224b712950eab7a1",
+    "url": "/static/js/main.c291a844.chunk.js"
+  },
+  {
+    "revision": "0a7197169ca4f0bcde0d",
+    "url": "/static/js/runtime-main.6ce4f2c8.js"
+  },
+  {
+    "revision": "c2e5d0c09ea4b664bff8977d27c2a171",
+    "url": "/static/media/bunny.c2e5d0c0.png"
+  }
+]);
