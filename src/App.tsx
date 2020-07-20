a368cb91934c132990fb549b3e94f5160da0a3f9
Rigwarl <Avips@mail.ru>
2020-07-20 20:19:45 +0300

    add circle draws

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Stage, Sprite } from '@inlet/react-pixi'
 import Camera from './Camera';
+import Circle from './Circle';
 
 import bunnyPng from './bunny.png';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Stage>
         <Camera>
+          {Array.from({ length: 10000 }, (_, i) => <Circle x={i * 5} y={i * 5} />)}
           <Sprite image={bunnyPng} />
           <Sprite image={bunnyPng} x={200} />
         </Camera>
